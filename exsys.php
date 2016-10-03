256a7ddade615607982128fbc319b7e2d0b34b6c
Matthew <myshorts.mail@gmail.com>
2016-10-03 16:41:25 +0200

    input manipulation mostly done

--- exsys.php
+++ exsys.php
@@ -1,15 +1,64 @@
 #!/usr/bin/php
 <?php
 
-$facts;
+require_once("exsys_facts.php");
+
+$reschars = array("+", "!", "^", "|", "=", "<", ">", "?", "(", ")", "#");
+$facts = array();
+$ifacts = array();
 $rules;
+$queries;
 $infile;
 
 if ($argc == 1)
+	exsys_error("ERROR: No input file specified.");
+$infile = $argv[1];
+if (!file_exists($infile))
+	exsys_error("ERROR: File \"" . $infile . "\" not found. Check that it exists.");
+if (filesize($infile) == 0)
+	exsys_error("ERROR: File \"" . $infile . "\" appears to be empty.");
+if (($queries = get_queries($infile)) == null)
+	exsys_error("ERROR: No queries specified.");
+// print_queries($queries);
+if (($ifacts = get_initial_facts($infile)) == null)
+	echo("WARNING: No facts are initially true!" . PHP_EOL);
+if (($facts = get_facts($infile, $ifacts)) == null)
+	exsys_error("ERROR: No facts found in file.");
+// print_facts($facts);
+
+
+function exsys_error($message)
 {
-	echo("No input file specified.");
+	echo($message . PHP_EOL);
 	exit(1);
 }
-$infile = $argv[1];
-echo($argv[1]);
+
+
+/*TEST FUNCS*/
+function print_facts(array $facts)
+{
+	foreach ($facts as $f)
+	{
+		print(key($f) . " = " . $f[key($f)] . PHP_EOL);
+	}
+}
+
+function print_queries(array $queries)
+{
+	print("Queries:" . PHP_EOL);
+	foreach ($queries as $q)
+	{
+		print($q . PHP_EOL);
+	}
+}
+
+function print_ifacts(array $ifacts)
+{
+	print("Initial Facts:" . PHP_EOL);
+	foreach ($ifacts as $if)
+	{
+		print($if . PHP_EOL);
+	}
+}
+
 ?>
---
+++ exsys_facts.php
@@ -0,0 +1,127 @@
+<?php
+
+function clean_line($str)
+{
+	$compos = strpos($str, "#");
+	$nstr = substr($str, 0, $compos);
+	return (preg_replace("/\s+/", "", $nstr));
+}
+
+function get_initial_facts($filename)
+{
+	$file;
+	$line;
+	$ifacts = array();
+
+	$file = fopen($filename, "r");
+	if ($file)
+	{
+		while (($line = fgets($file)) == TRUE)
+		{
+			if ($line[0] == '=')
+			{
+				$line = clean_line($line);
+				for ($cnt = 0; $cnt < strlen($line); $cnt++)
+				{
+					$char = substr($line, $cnt, 1);
+					if (ctype_upper($char) == TRUE)
+					{
+						if (count($ifacts) == 0)
+							$ifacts[0] = $char;
+						else
+							array_push($ifacts, $char);
+					}
+				}
+			}
+		}
+	}
+	fclose($file);
+	if (count($ifacts) == 0)
+		return (null);
+	else
+		return ($ifacts);
+}
+
+function get_queries($filename)
+{
+	$file;
+	$line;
+	$queries = array();
+	$char;
+
+	$file = fopen($filename, "r");
+	if ($file)
+	{
+		while (($line = fgets($file)) == TRUE)
+		{
+			if ($line[0] == '?')
+			{
+				$line = clean_line($line);
+				for ($cnt = 0; $cnt < strlen($line); $cnt++)
+				{
+					$char = substr($line, $cnt, 1);
+					if (ctype_upper($char) == TRUE)
+					{
+						if (count($queries) == 0)
+							$queries[0] = $char;
+						else
+							array_push($queries, $char);
+					}
+				}
+			}
+		}
+	}
+	fclose($file);
+	if (count($queries) == 0)
+		return (null);
+	else
+		return ($queries);
+}
+
+function get_facts($filename, $ifacts)
+{
+	$file;
+	$line;
+	$facts = array();
+	$fact = array();
+	$char;
+	$state = 0;
+
+	$file = fopen($filename, "r");
+	if ($file)
+	{
+		while (($line = fgets($file)) == TRUE)
+		{
+			// if ($line[0] != '?' && $line[0] != '=' && (empty($line)) == FALSE)
+			// {
+			$line = clean_line($line);
+			for ($cnt = 0; $cnt < strlen($line); $cnt++)
+			{
+				$char = substr($line, $cnt, 1);
+				if (ctype_upper($char) == TRUE)
+				{
+					if (($ifacts != null) && (in_array($char, $ifacts)) == TRUE)
+						$state = 1;
+					else
+						$state = 0;
+					$fact = array($char => $state);
+					if (in_array($fact, $facts) == FALSE)
+					{
+						if (count($facts) == 0)
+							$facts[0] = $fact;
+						else
+							array_push($facts, $fact);
+					}
+				}
+			}
+			// }
+		}
+	}
+	fclose($file);
+	if (count($facts) == 0)
+		return (null);
+	else
+		return ($facts);
+}
+
+?>
